789a6790d5360a2e58cad723226fc661d2b27ed7
anapaulamariano <mariano.oliveira.ana@escola.pr.gov.br>
2025-08-23 11:47:47 -0300

    13

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ botaoMostraPalavras.addEventlistener('click',mostraPalavrasChave);
 function mostraPalavrasChave{
    const texto = document.querySelector('#entrada-d-texto').value;
    const campoResultado = document.querySelector("#resultado-palarachave");
-
-   campoResultado.textContent= texto;
+   const palavrasChave = texto.split('');
+   
+   campoResultado.textContent= palavrasChave.join(",");
 }
+
+    function processaTexto(texto);{
+        let palavras = texto.split(/p{L}+u/);
+       
+       
+       let frequencias = [];
+       for(let i in palavras){
+        frequencias[i] = 0;
+        for (ler j of in palavras){
+            if (palavras[i]==palavras[j]){
+                frequencias[i]++;
+
+            }
+        }
+    } 
+       
+     console.log(frequencias);
+
+           
+     return palavras;
+    }
